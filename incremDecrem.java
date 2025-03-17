4be20d25c016944da34038392657d71966f44ce3
RENATA SILVA BEZERRA <rsbezerra25@gmail.com>
2025-03-17 16:25:39 -0300

    Create incremDecrem.java
    Conceitos de incremento e decremento

---
+++ incremDecrem.java
@@ -0,0 +1,10 @@
+public class incrementation {
+    public static void main(String[] args) {
+        int a = 10;
+        
+        System.out.println("a++ vai atribuir a variável e depois incrementar = " + a++);
+        System.out.println("++a vai incrementar a variavel e depois atribuir = " + ++a);
+        System.out.println("a-- vai atribuir a variável e depois decrementar = " + a--);
+        System.out.println("--a vai decrementar a variavel e depois atribuir = " + --a);
+    }
+} 
