bdda9e14de1befca663301e0b8ba3318674eba95
RENATA SILVA BEZERRA <rsbezerra25@gmail.com>
2025-03-26 05:08:38 -0300

    Update Main.java

--- Java-bolado/src/main/java/org/example/Main.java
+++ Java-bolado/src/main/java/org/example/Main.java
@@ -33,3 +33,50 @@ public class Main {
         System.out.println("Array final " + array);
     }
 }
+
+
+/* 
+É fornecida uma matriz A não vazia, composta por N inteiros.
+
+O array A representa uma lista encadeada. Uma lista é construída a partir deste array da seguinte forma:
+
+o primeiro nó (a cabeça) está localizado no índice 0;
+o valor de um nó localizado no índice K é A[K];
+se o valor de um nó for −1 então ele é o último nó da lista;
+caso contrário, o sucessor de um nó localizado no índice K está localizado no índice A[K] (você pode assumir que A[K] é um índice válido, ou seja, 0 ≤ A[K] < N).
+Por exemplo, para a matriz A tal que:
+
+  Um[0] = 1
+  Um[1] = 4
+  Um[2] = -1
+  Um[3] = 3
+  Um[4] = 2
+
+
+a seguinte lista é construída:
+
+o primeiro nó (a cabeça) está localizado no índice 0 e tem valor 1;
+o segundo nó está localizado no índice 1 e tem valor 4;
+o terceiro nó está localizado no índice 4 e tem valor 2;
+o quarto nó está localizado no índice 2 e tem um valor de -1.
+Escreva uma função:
+
+classe Solução { public int solução(int[] A); }
+
+que, dado um array não vazio A consistindo de N inteiros, retorna o comprimento da lista construída a partir de A da maneira acima.
+
+Por exemplo, dado o array A tal que:
+
+  Um[0] = 1
+  Um[1] = 4
+  Um[2] = -1
+  Um[3] = 3
+  Um[4] = 2
+a função deve retornar 4, conforme explicado no exemplo acima.
+
+Suponha que:
+
+N é um inteiro dentro do intervalo [ 1 .. 200.000 ];
+cada elemento da matriz A é um inteiro dentro do intervalo [ −1 .. N-1 ];
+sempre será possível construir a lista e seu comprimento será finito.
+*/
